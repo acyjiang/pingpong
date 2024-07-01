5429254535d34a67b85a46e8ff9166d3c79c2daa
Andy Jiang <acyjiang@mit.edu>
2024-06-30 23:22:30 -0500

    modify chatbot seeding

--- bots/Chatbot.py
+++ bots/Chatbot.py
@@ -3,40 +3,44 @@ from groq import Groq
 class Chatbot:
     def __init__(self, args):
         self.initialize_client(api_key=args["api_key"])
-
-        self.messages = []
-        self.plant_seed(seed=args["seed"])
-
         self.model = args["model"]
 
     def initialize_client(self, api_key):
         self.client = Groq(
             api_key=api_key,
         )
+        self.messages = []
 
     def plant_seed(self, seed):
         if len(self.messages) > 0:
             raise Exception("Should only be called when Chatbot is initialized")
         
-        if seed:
-            self.messages.append({
-                "role": "user",
-                "content": seed,
-            })
-
-    def append_message_to_conversation(self, message):
         self.messages.append({
-            "role": "user",
-            "content": message,
+            "role": "system",
+            "content": seed,
         })
 
-    def generate_response(self, input):
-        self.append_message_to_conversation(input)
+    def append_input_to_conversation(self, input):
+        self.messages.append({
+            "role": "user",
+            "content": input,
+        })
+
+    def append_output_to_conversation(self, output):
+        self.messages.append({
+            "role": "assistant",
+            "content": output,
+        })
+
+    def generate_output(self, input):
+        self.append_input_to_conversation(input)
 
         chat_completion = self.client.chat.completions.create(
             messages=self.messages,
             model=self.model,
         )
+        output = chat_completion.choices[0].message.content
+        self.append_output_to_conversation(output)
 
-        return chat_completion.choices[0].message.content
+        return output
     
